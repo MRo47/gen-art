2c972d61197795d7ebc7a6a8e156d8bd3d21356f
MRo47 <rodriguesmyron47@gmail.com>
2024-05-19 18:24:35 +0100

    add particle size factor

--- flow-fields/particles.js
+++ flow-fields/particles.js
@@ -5,8 +5,8 @@ class Particle {
     this.speed = _speed;
     this.color = color(random(0, 200), random(0, 200), 255);
     this.col_switch = false;
-    this.loc.z = random(1, 8);
-
+    let max_d = max(width, height);
+    this.loc.z = random(0.0005 * max_d, 0.004 * max_d);
   }
   run() {
     this.move();
